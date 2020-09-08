656bfef9e9a9d3a2883862741e48d0f020b0bd1e
gpablosl <gpablosleon15@gmail.com>
2020-09-08 08:23:40 -0700

    class 2

--- src/Hero.cc
+++ src/Hero.cc
@@ -1,11 +1,15 @@
 #include "Hero.hh"
 
-Hero::Hero(short int health, uint32_t strength, uint32_t exp, short int luck, uint32_t speed)
+Hero::Hero(short int health, uint32_t strength, uint32_t exp, short int luck, uint32_t speed, uint32_t level, std::string* skills)
 :Character(health, strength, exp, luck, speed)
 {
-    std::cout << "Health: "<< GetHealth() << std::endl;
-    std::cout <<  "Exp: " << GetExp()  << std::endl;
-    std::cout <<  "Luck: " << GetLuck() << std::endl;
-    std::cout <<  "Strength: " << GetStrenght() << std::endl;
-    std::cout <<  "Speed: " << GetSpeed() << std::endl;
+    this->level=level;
+    this->skills=skills;
+
 }
+
+std::string* Hero::GetSkills() const
+{
+    return skills;
+}
+
--- src/main.cc
+++ src/main.cc
@@ -4,25 +4,36 @@
 
 int main()
 {
+    Hero* hero1{new Hero(5, 3, 1, 2, 3, 1 , new std::string[4]{{"fire ball"},{"fenix tail"},{"war growl"},{"fly"}})};
 
-    Hero* hero1{new Hero(5, 3, 1, 2, 3)};
-    hero1->SetLore("la chinita en el bosque se perdio");
-    std::cout << "Lore: : " << hero1->GetLore() << std::endl;
-
-
-    /*Character* character1{new Character(5, 3, 1, 2, 3)};
+    /*
+    Hero** heroList
+    {
+        new Hero*[4]
+        {
+            new Hero(5, 3, 1, 2, 3, 1 , new std::string[4]{{"fire ball"},{"fenix tail"},{"war growl"},{"fly"}}),
+            new Hero(4, 4, 9, 5, 2, 3 , new std::string[4]{{"water heal"},{"erik do something!!"},{"me como un burrito"},{"ray sword"}}),
+            new Hero(7, 2, 1, 7, 4, 4 , new std::string[4]{{"lorem"},{"ipsum"},{"XD"},{"no"}}),
+            new Hero(8, 4, 3, 9, 7, 8 , new std::string[4]{{"este wey no hace nada"},{"nada"},{"nada 2"},{"dormir"}})
+        }
+    };
+    */
 
     std::cout << "Character info:" << std::endl;
-    std::cout << "Health: " << character1->GetHealth() << std::endl;
-    std::cout << "Exp: " << character1->GetExp() << std::endl;
-    std::cout << "Luck:" << character1->GetLuck() << std::endl;
-    std::cout << "Speed: " << character1->GetSpeed() << std::endl;
-    std::cout << "Streght: " << character1->GetStrenght() << std::endl;
-    character1->SetLore("la chinita en el bosque se perdio");
-    character1->GetDamage(2);
-    std::cout << "Health: " << character1->GetHealth() << std::endl;
-    std::cout << "Lore: : " << character1->GetLore() << std::endl;
-    delete &character1; */
+    for(int i{};i<4;i++)
+    {
+         std::cout << "Skill " << i+1 << ": " <<*(hero1->GetSkills()+i) << std::endl;
+    }
+    std::cout << "Health: " << hero1->GetHealth() << std::endl;
+    std::cout << "Exp: " << hero1->GetExp() << std::endl;
+    std::cout << "Luck:" << hero1->GetLuck() << std::endl;
+    std::cout << "Speed: " << hero1->GetSpeed() << std::endl;
+    std::cout << "Streght: " << hero1->GetStrenght() << std::endl;
+    hero1->SetLore("la chinita en el bosque se perdio");
+    hero1->GetDamage(2);
+    std::cout << "Health: " << hero1->GetHealth() << std::endl;
+    std::cout << "Lore: : " << hero1->GetLore() << std::endl;
+    delete &hero1; 
 
     std::cin.get();
     return 0;
